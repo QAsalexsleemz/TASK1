5d36ac4f49c8c7e9c26b28b2fe8f6b4ca1008090
QAsalexsleemz <salim.adeshina@gmail.com>
2025-05-19 23:33:57 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const firstDayoftheweek = 'Monday'
+const secondDayoftheweek = 'Tuesday'
+const thirdDayoftheweek = 'Wednesday'
+// const fourthDayoftheweek = 'Thursday'
+// const fifthDayoftheweek = 'Friday'
+// const sixthDayoftheweek = 'Saturday'
+// const seventhDayoftheweek = 'Sunday'
+
+const currentDayoftheweek = 'Tuesday'
+
+console.log(firstDayoftheweek)
+console.log(secondDayoftheweek)
+console.log(thirdDayoftheweek)
+console.log(currentDayoftheweek)
+console.log(typeof(currentDayoftheweek))
+
+const mondayPosition = 1
+const tuesdayPosition = 2
+
+console.log(tuesdayPosition)
+console.log(typeof(tuesdayPosition))
+
+const output = 'Today is ' + currentDayoftheweek + '.' + ' It is the ' + tuesdayPosition + 'nd' + ' day of the week.'
+
+console.log(output)
