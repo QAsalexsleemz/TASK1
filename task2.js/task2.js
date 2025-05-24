6fda73125439c6f416341cb5216dc91f4d3cd1bc
QAsalexsleemz <salim.adeshina@gmail.com>
2025-05-24 14:18:26 -0700

    first commit

---
+++ task2.js/task2.js
@@ -0,0 +1,25 @@
+// MODULUS OPERATOR
+// let number = 8
+    number = 7
+    number = 10
+
+// if(number = 8 % 2 === 0){
+//     console.log("number is even")
+// }
+// else{
+//     console.log('number is even')
+// }
+
+if(number = 7 % 2 === 0){
+    console.log("number is even")
+}
+else{
+    console.log("number is odd")
+}
+
+if(number = 10 % 2 === 0){
+    console.log("number is even")
+}
+else{
+    console.log("number is odd")
+}
